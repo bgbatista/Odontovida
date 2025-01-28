b67c2476d3af912e3554844d1774143cbdf0a31a
Batista <mariabeatrizbatistagabriel@gmail.com>
2025-01-28 17:22:10 -0300

    Create Criando Banco de Dados.sql

---
+++ Criando Banco de Dados.sql
@@ -0,0 +1,11 @@
+/*
+SENAC DF - CEP Jessé Freire
+2024.07.274 - Aprendizagem Proficional de Qualificação em Desenvolvimento de Softwares
+Prof° Hudson Neves 
+Projeto Final - Implemenetação de Banco de Dados Relacional SQL 
+Projeto - Clinica Odontológica OdontoVida*/
+
+-- 1. Criação do Arquivo/Banco de Dados
+create database OdontoVida;
+-- 2. Acessando Arquivo/Banco de Dados 
+use OdontoVida;
